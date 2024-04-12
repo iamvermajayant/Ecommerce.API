8c35c6679b8316eb8dff96cf3c47000221b5c2d2
Jayant Verma <iamvermajayant@gmail.com>
2024-04-12 22:27:20 +0530

    ➕ added stats route for admin

--- routes/user.js
+++ routes/user.js
@@ -58,4 +58,33 @@ router.get('/alluser', async (req,res)=> {
 })
 
 
+
+//user Stats 
+router.get('/stats' , verifyTokenWithAdmin, async (req, res) => {
+    const date = new Date();
+    const lastYear = new Date(date.setFullYear(date.getFullYear() - 1));
+
+    try {
+        const data = await User.aggregate([
+            {$match : {createdAt : {$gte : lastYear}}},
+            {
+                $project : {
+                    month : {$month : "$createdAt"},
+                },
+            },
+            {
+                $group : {
+                    _id : "$month",
+                    total : {$sum : 1},
+                },
+            },
+        ])
+
+        res.status(200).json(data);
+    } catch (error) {
+        res.status(500).json(error)
+    }
+})
+
+
 module.exports = router;
